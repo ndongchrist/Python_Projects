28aa745107c971db3efb1ab90d0d8ea5ae7eebed
Akashiutchiha <94064299+Akashiutchiha@users.noreply.github.com>
2022-01-12 12:13:31 -0800

    added Borther's num

--- ClassEx.py
+++ ClassEx.py
@@ -3,6 +3,7 @@ Msg = '''
 My mother has phone number +237651425227
 my father has phone number +2376.99.65.60.49
 my sister has phone number +2376-99-35-71-80
+my brother's number number +237659992528
 '''
 chain = r"^\+2376([.- ]?[0-9]{2}){4}$"
 ChainCom = re.compile(chain)
