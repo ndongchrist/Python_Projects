ef9a927b51090d8f8611c0ebe24258004fc571da
Akashiutchiha <94064299+Akashiutchiha@users.noreply.github.com>
2022-01-12 12:26:09 -0800

    not important

--- ClassEx.py
+++ ClassEx.py
@@ -5,7 +5,7 @@ my father has phone number +2376.99.65.60.49
 my sister has phone number +2376-99-35-71-80
 my brother's number number +237659992528
 '''
-chain = r"^\+2376([.- ]?[0-9]{2}){4}$"
+chain = r"^+2376([.- ]?[0-9]{2}){4}$"
 ChainCom = re.compile(chain)
 matches = ChainCom.finditer(Msg)
 for match in matches:
